10852fdf16da2b784b351263b04848d0ec8e962f
LucasPar08 <lucaspark2008@gmail.com>
2025-04-15 12:01:43 -0300

    Simulacro terminado

---
+++ Assets/ColoniaDeVacaciones.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+public class ColoniaVacaciones : MonoBehaviour
+{
+    public int Juveniles;
+    public int Infantiles;
+
+    void Start()
+    {
+        // Start is called before the first frame
+        if (Juveniles < 0 || Juveniles > 100 || Infantiles < 0 || Infantiles > 100)
+        {
+            Debug.LogError("Error: Tiene que ser un numero menor o igual a 100.");
+            return;
+        }
+
+        int profesoresJuveniles = 0;
+        if (Juveniles >= 20)
+        {
+            profesoresJuveniles = Juveniles / 20;
+        }
+        int profesoresInfantiles = 0;
+        if (Infantiles >= 10)
+        {
+            profesoresInfantiles = Infantiles / 10;
+        }
+
+        int listaEsperaJuveniles = Juveniles - (profesoresJuveniles * 20);
+        int listaEsperaInfantiles = Infantiles - (profesoresInfantiles * 10);
+        int totalListaEspera = listaEsperaJuveniles + listaEsperaInfantiles;
+
+        int totalProfesores = profesoresJuveniles + profesoresInfantiles;
+        int coordinadores = (totalProfesores + 4) / 5;
+
+        Debug.Log("Se necesitan " + profesoresJuveniles + " profesores para los inscriptos juveniles.");
+        Debug.Log("Se necesitan " + profesoresInfantiles + " profesores para los inscriptos infantiles.");
+        Debug.Log("Se necesitan " + coordinadores + " coordinadores.");
+        Debug.Log("Inscriptos en lista de espera: " + totalListaEspera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+x
+
